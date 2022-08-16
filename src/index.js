07d2b75245f0d2ca0dc8420f45b530aa1f7f9ae7
tharunoptimus <tharunoptimus@outlook.com>
2022-08-17 00:15:28 +0530

    Made the TicTacToe box a little rounded rectangle Added more styles

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ class Board extends React.Component {
 	render() {
 		return (
 			<div>
-				<div className="board-row">
+				<div className="board-row first-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
 					{this.renderSquare(2)}
@@ -33,7 +33,7 @@ class Board extends React.Component {
 					{this.renderSquare(4)}
 					{this.renderSquare(5)}
 				</div>
-				<div className="board-row">
+				<div className="board-row last-row">
 					{this.renderSquare(6)}
 					{this.renderSquare(7)}
 					{this.renderSquare(8)}
